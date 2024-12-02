677fa2f01f435128b60c09510e56e5abef9cbafe
Ingo Junefjord <lord.ingo@gmail.com>
2024-12-02 09:20:23 +0100

    Day 2: works

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -4,157 +4,88 @@ enum Direction {
     Neither,
 }
 
+fn strictly_monotonic_with_max_change(input: &[u32], max_change: u32) -> Result<(), usize> {
+    let mut prev = None;
+    let mut direction = Direction::Neither;
+    for (index, num) in input.iter().enumerate() {
+        if let Some(prev) = prev {
+            match num.cmp(prev) {
+                std::cmp::Ordering::Less => {
+                    match direction {
+                        Direction::Increasing => return Err(index),
+                        Direction::Decreasing => {}
+                        Direction::Neither => direction = Direction::Decreasing,
+                    }
+                    if (prev - num) > max_change {
+                        return Err(index);
+                    }
+                }
+                std::cmp::Ordering::Greater => {
+                    match direction {
+                        Direction::Decreasing => return Err(index),
+                        Direction::Increasing => {}
+                        Direction::Neither => direction = Direction::Increasing,
+                    }
+                    if (num - prev) > max_change {
+                        return Err(index);
+                    }
+                }
+                std::cmp::Ordering::Equal => return Err(index),
+            }
+        }
+        prev = Some(num);
+    }
+    Ok(())
+}
+
 pub fn run(input: String) -> (String, String) {
     let mut safe_reports = 0;
-    'outer: for line in input.lines() {
-        let mut prev: Option<u32> = None;
-        let mut direction = Direction::Neither;
-        for num in line.split_ascii_whitespace().map(|x| x.parse().unwrap()) {
-            if let Some(prev) = prev {
-                match direction {
-                    Direction::Increasing => {
-                        if num > prev {
-                            match num - prev {
-                                1..=3 => (),
-                                _ => continue 'outer,
-                            }
-                        } else {
-                            continue 'outer;
-                        }
-                    }
-                    Direction::Decreasing => {
-                        if num < prev {
-                            match prev - num {
-                                1..=3 => (),
-                                _ => continue 'outer,
-                            }
-                        } else {
-                            continue 'outer;
-                        }
-                    }
-                    Direction::Neither => {
-                        if num > prev {
-                            direction = Direction::Increasing;
-                            match num - prev {
-                                1..=3 => (),
-                                _ => continue 'outer,
-                            }
-                        } else if num < prev {
-                            direction = Direction::Decreasing;
-                            match prev - num {
-                                1..=3 => (),
-                                _ => continue 'outer,
-                            }
-                        } else {
-                            continue 'outer;
-                        }
-                    }
-                }
-            }
-            prev = Some(num);
-        }
-
-        safe_reports += 1;
-    }
-
     let mut safe_reports_2 = 0;
-    'outer: for line in input.lines() {
-        let mut first_miss = true;
-        let mut prev: Option<u32> = None;
-        let mut direction = Direction::Neither;
-        for num in line.split_ascii_whitespace().map(|x| x.parse().unwrap()) {
-            let mut miss = false;
-            if let Some(prev) = prev {
-                match direction {
-                    Direction::Increasing => {
-                        if num > prev {
-                            match num - prev {
-                                1..=3 => (),
-                                _ => {
-                                    if first_miss {
-                                        first_miss = false;
-                                        miss = true;
-                                    } else {
-                                        continue 'outer;
-                                    };
-                                }
-                            }
-                        } else if first_miss {
-                            first_miss = false;
-                            miss = true;
-                        } else {
-                            continue 'outer;
-                        }
+    let max_change = 3;
+
+    for line in input.lines() {
+        let nums = line
+            .split_ascii_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect::<Vec<u32>>();
+        match strictly_monotonic_with_max_change(&nums, max_change) {
+            Ok(_) => safe_reports += 1,
+            Err(index) => {
+                let mut vec1 = nums.clone();
+                let mut vec2 = nums.clone();
+                let mut vec3 = nums.clone();
+                vec1.remove(index);
+                vec2.remove(index - 1);
+                if index >= 2 {
+                    vec3.remove(index - 2);
+                }
+
+                match strictly_monotonic_with_max_change(&vec1, max_change) {
+                    Ok(_) => {
+                        safe_reports_2 += 1;
+                        continue;
                     }
-                    Direction::Decreasing => {
-                        if num < prev {
-                            match prev - num {
-                                1..=3 => (),
-                                _ => {
-                                    if first_miss {
-                                        first_miss = false;
-                                        miss = true;
-                                    } else {
-                                        continue 'outer;
-                                    }
-                                }
-                            }
-                        } else if first_miss {
-                            first_miss = false;
-                            miss = true;
-                        } else {
-                            continue 'outer;
-                        }
+                    Err(_) => (),
+                }
+                match strictly_monotonic_with_max_change(&vec2, max_change) {
+                    Ok(_) => {
+                        safe_reports_2 += 1;
+                        continue;
                     }
-                    Direction::Neither => {
-                        if num > prev {
-                            direction = Direction::Increasing;
-                            match num - prev {
-                                1..=3 => (),
-                                _ => {
-                                    if first_miss {
-                                        first_miss = false;
-                                        miss = true;
-                                    } else {
-                                        if first_miss {
-                                            first_miss = false;
-                                            miss = true;
-                                        } else {
-                                            continue 'outer;
-                                        };
-                                    }
-                                }
-                            }
-                        } else if num < prev {
-                            direction = Direction::Decreasing;
-                            match prev - num {
-                                1..=3 => (),
-                                _ => {
-                                    if first_miss {
-                                        first_miss = false;
-                                        miss = true;
-                                    } else {
-                                        continue 'outer;
-                                    }
-                                }
-                            }
-                        } else {
-                            if first_miss {
-                                first_miss = false;
-                                miss = true;
-                            } else {
-                                continue 'outer;
-                            };
-                        }
+                    Err(_) => (),
+                }
+                match strictly_monotonic_with_max_change(&vec3, max_change) {
+                    Ok(_) => {
+                        safe_reports_2 += 1;
                     }
+                    Err(_) => (),
                 }
             }
-            if !miss {
-                prev = Some(num);
-            }
         }
-
-        safe_reports_2 += 1;
     }
-    (format!("{safe_reports}"), format!("{safe_reports_2}"))
+
+    (
+        format!("{safe_reports}"),
+        format!("{}", safe_reports + safe_reports_2),
+    )
 }
